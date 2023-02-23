2a7d3ae173d877d02d617f8e7942db07b9127b9b
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-02-22 23:13:59 -0600

    Add files via upload

---
+++ Colas/src/colas/Colas.java
@@ -0,0 +1,13 @@
+
+package colas;
+/**
+ *
+ * @author Investor_DM_
+ */
+public class Colas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
